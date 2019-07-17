f245727ecbffdd496b24e50167f7b12b17268389
Daniel Sikar <dsikar@gmail.com>
2019-07-17 13:12:17 +0000

    ID feature prototype

---
+++ xzm/read-id.py
@@ -0,0 +1,11 @@
+def readID():
+	f = open("panel-id.txt", "r");
+	panel_id = f.readline();
+	f.close;
+	return panel_id;
+
+pid = readID();
+URL = "http://ipaddress/service.php?action=r&id=" + pid;
+print URL;
+
+
