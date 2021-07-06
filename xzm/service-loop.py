db332192854a8632a18a5e30cbf3da350832f43a
Daniel Sikar <dsikar@gmail.com>
2021-07-06 14:20:55 +0100

    Added a reference log

--- xzm/service-loop.py
+++ xzm/service-loop.py
@@ -1,60 +1,61 @@
-#!/usr/bin/env python
-import urllib2
-#import commands
 import subprocess
 import shlex
 import time
 import datetime
-import readID as readIDVar;
-#current script
-counter = 0;
-consecutiveRuns = 0;
-timeDelay = 0
+import readID as readIDVar
+
+def writelog(entry, logfile) :
+    """
+    writelog - write log entry to log file
+    to usb drive is exists, otherwise to /tmp
+    Input
+        entry: string, log entry
+        logfile: string, log file
+    Output
+        none
+    """
+    logdrive = ''
+    out = subprocess.check_output(['ls', '/media/pi'])
+    if len(out) > 0 :
+        logdrive = '/media/pi/' + out
+        logdrive = logdrive.rstrip('\n')
+        logdrive += '/'
+    else :
+        logdrive = '/tmp/'
+    logdrive += logfile
+    fout = open(logdrive, 'a')
+    fout.write(entry)
+    fout.close()
+
+timeDelay = 1
 
 pid = readIDVar.readID();
 pid = pid.strip()
-print("The PI id is " + str(pid))
+entry = "Logging Panel PI id: " + str(pid) + '\nTODO scan devices, decode packets\n'
+print(entry)
+f = open('packet.txt')
+packets = f.readlines()
+
+logfile = str(datetime.datetime.today().strftime('%Y%m%d%H%M%S'))
+logfile += '_BlackBox.log'
+writelog(entry, logfile)
 
 while(True):
-    startTime = datetime.datetime.now();
 
-    readURL = "http://3.8.97.201/service.php?action=r&id=" + str(pid)
-    response = urllib2.urlopen(readURL)
-    #print(readURL);
-    content = response.read();
-    counter = counter + 1;
-    print("\nLoop "+str(counter));
-    print "Packet in jobs list : \n"+content
+    for packet in packets :
+        startTime = datetime.datetime.now();
 
-    if(len(content) > 1):
-	consecutiveRuns = 0;
-        action = "python /var/www/html/MXSpeak/packetizer/pointinfotest.py " + content;
+        content =  packet #+ '\r\n' #.strip()
+        # print "Packet in jobs list : \n"+content
+
+        action = "python /home/pi/packetizer/pointinfotest.py " + content;
+        #print(action)
         payload = subprocess.check_output(shlex.split(action));
         if len(payload) > 1: #for some reason printing an empty payload still constitutes to greater than 0, so we use 1 instead
-            print "Payload response to serviced packet : \n"+payload; 
-            reply = "http://3.8.97.201/service.php?action=w&id="+str(pid)+"&payload=" + str(payload)
-            #reply = "http://3.9.146.52/service.php?action=w"+"&payload=" + str(payload) +"&id=" +str(pid)
-	    #reply = urllib2.quote(reply)
-	    print(reply)
-            response = urllib2.urlopen(reply)
+            writelog(payload,logfile)
+            #print "Payload response to serviced packet : \n"+payload; 
 	else:
-	    print("this is the payload" + str(payload))
+	    #print("this is the payload" + str(payload))
             print "No device recorded for this point",
 
-    else:
-        consecutiveRuns = consecutiveRuns + 1;
-
-    #Currently this condition will assign timeDelay redundantly, should be made more efficient.
-    if(consecutiveRuns > 30):
-        timeDelay = 1.8;   #sleep for 1.8seconds due to period of inactivity;
-    else:
-        timeDelay = 0.08;  #if a http request comes in then decrease the delay as we expect further traffic to arrive.
-
-    time.sleep(timeDelay);
-
-    endTime = datetime.datetime.now();
-    difference = endTime - startTime;
-    difference = (difference.total_seconds() * 1000);
-
-    print("Time taken to run Loop " + str(counter)+ " = " + str(difference));
-
+        time.sleep(timeDelay) 
